ad9a3e23a3d82fdd048abf85eb39c8cdcaddb206
StegmayerTobias <tobiassebastianstegmayer@gmail.com>
2025-11-06 09:41:00 -0300

    Barra lateral

--- gis/main.js
+++ gis/main.js
@@ -1,17 +1,17 @@
-import './style.css';
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import "./style.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
 
 const map = new Map({
-  target: 'map',
+  target: "map",
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
+      source: new OSM(),
+    }),
   ],
   view: new View({
     center: [0, 0],
-    zoom: 2
-  })
+    zoom: 2,
+  }),
 });
