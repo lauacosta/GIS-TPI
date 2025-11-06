471b92225637826d9b9ef8a12ac0bf0cb229a5be
StegmayerTobias <tobiassebastianstegmayer@gmail.com>
2025-11-06 12:46:07 -0300

    Menu lateral y custom control para zoom in y out

--- gis/main.js
+++ gis/main.js
@@ -4,7 +4,7 @@ import TileLayer from "ol/layer/Tile";
 import { Map, View } from "ol";
 import { fromLonLat } from "ol/proj";
 
-new Map({
+const map = new Map({
   target: "map",
   layers: [
     new TileLayer({
@@ -16,3 +16,23 @@ new Map({
     zoom: 5,
   }),
 });
+
+const menuBtn = document.getElementById("menu");
+const aside = document.querySelector("aside");
+
+menuBtn.addEventListener("click", () => {
+  aside.classList.toggle("menu-open");
+  // aside.style.left = "-10%";
+});
+
+document.getElementById("zoom-out").onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom - 1);
+};
+
+document.getElementById("zoom-in").onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom + 1);
+};
