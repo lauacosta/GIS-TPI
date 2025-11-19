b54d6e309487d16a6692beb37ff09d6875235c9d
StegmayerTobias <tobiassebastianstegmayer@gmail.com>
2025-11-19 16:02:24 -0300

    Agregue nuevos iconos y estilos a los iconos

--- gis/src/ui/toolbar.js
+++ gis/src/ui/toolbar.js
@@ -6,6 +6,8 @@ export function initToolbar(map, interactionControls) {
     zoomout: document.getElementById("zoom-out"),
     zoomin: document.getElementById("zoom-in"),
     query: document.getElementById("query"),
+    draw: document.getElementById("draw"),
+    measure: document.getElementById("measure"),
   };
 
   dom.center.addEventListener("click", () => {
@@ -18,6 +20,14 @@ export function initToolbar(map, interactionControls) {
 
   dom.zoomin.addEventListener("click", () => {
     zoomIn(map.getView());
+  });
+
+  dom.draw.addEventListener("click", () => {
+    alert("CHAQUE. Aún no esta implementado. Anda a UI/toolbar");
+  });
+
+  dom.measure.addEventListener("click", () => {
+    alert("CHAQUE. Aún no esta implementado. Anda a UI/toolbar");
   });
 
   if (dom.query) {
